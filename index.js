fa8235b4492f921604acb25817860dee078f6bb6
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2019-09-18 14:28:00 -0400

    Add support for custom `importer` in plugin opts

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // tooling
+import createImporter from './lib/createImporter';
 import mergeSourceMaps from './lib/merge-source-maps';
 import postcss from 'postcss';
-import sassResolve from '@csstools/sass-import-resolve';
-import sass from 'sass';
-import { dirname, resolve as pathResolve } from 'path';
 
 // transform css with sass
 export default postcss.plugin('postcss-sass', opts => (root, result) => {
@@ -17,59 +15,30 @@ export default postcss.plugin('postcss-sass', opts => (root, result) => {
 	const includePaths = [].concat(opts && opts.includePaths || []);
 
 	// sass engine to use
-	const sassEngine = opts && opts.sass || sass
+	const sassEngine = opts && opts.sass || require('sass');
 
-	// sass resolve cache
-	const cache = {};
+	const importer = opts && opts.importer || createImporter({ includePaths, result });
 
 	return new Promise(
 		// promise sass results
-		(resolve, reject) => sassEngine.render(
-			// pass options directly into node-sass
-			Object.assign({}, opts, requiredSassConfig, {
-				file: `${postConfig.from}#sass`,
-				outFile: postConfig.from,
-				data: postCSS,
-				importer(id, parentId, done) {
-					// resolve the absolute parent
-					const parent = pathResolve(parentId);
-
-					// cwds is the list of all directories to search
-					const cwds = [dirname(parent)].concat(includePaths).map(includePath => pathResolve(includePath));
-
-					cwds.reduce(
-						// resolve the first available files
-						(promise, cwd) => promise.catch(
-							() => sassResolve(id, { cwd, cache, readFile: true })
-						),
-						Promise.reject()
-					).then(
-						({ file, contents }) => {
-							// push the dependency to watch tasks
-							result.messages.push({ type: 'dependency', file, parent });
-
-							// pass the file and contents back to sass
-							done({ file, contents });
-						},
-						importerError => {
-							// otherwise, pass the error
-							done(importerError);
-						}
-					);
-				}
-			}),
-			(sassError, sassResult) => sassError ? reject(sassError) : resolve(sassResult)
-		)
-	).then(
-		({ css: sassCSS, map: sassMap }) => mergeSourceMaps(
-			postMap.toJSON(),
-			JSON.parse(sassMap)
-		).then(prev => {
+		(resolve, reject) =>
+			sassEngine.render(
+				// pass options directly into node-sass
+				Object.assign({}, opts, requiredSassConfig, {
+					file: `${postConfig.from}#sass`,
+					outFile: postConfig.from,
+					data: postCSS,
+					importer,
+				}),
+				(sassError, sassResult) => sassError ? reject(sassError) : resolve(sassResult)
+			)
+	).then(({ css: sassCSS, map: sassMap }) =>
+		mergeSourceMaps(postMap.toJSON(), JSON.parse(sassMap)).then(prev => {
 			// update root to post-node-sass ast
 			result.root = postcss.parse(
 				sassCSS.toString(),
 				Object.assign({}, postConfig, {
-					map: { prev }
+					map: { prev },
 				})
 			);
 		})
@@ -80,12 +49,12 @@ const requiredPostConfig = {
 	map: {
 		annotation: false,
 		inline: false,
-		sourcesContent: true
-	}
+		sourcesContent: true,
+	},
 };
 
 const requiredSassConfig = {
 	omitSourceMapUrl: true,
 	sourceMap: true,
-	sourceMapContents: true
+	sourceMapContents: true,
 };
